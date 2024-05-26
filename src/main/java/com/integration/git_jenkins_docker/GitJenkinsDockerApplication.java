418ee3681a091d78f7e05f9c1df8325035e4cf2d
priyanka <priyankakushwaha7762@gmail.com>
2024-05-26 14:20:48 +0530

    first commit

---
+++ src/main/java/com/integration/git_jenkins_docker/GitJenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.integration.git_jenkins_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitJenkinsDockerApplication.class, args);
+	}
+
+}
